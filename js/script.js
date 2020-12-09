f7cdfc5b54a90c126033fa37ca93d9c0d8d95fcd
Simone <simone.sichili@gmail.com>
2020-12-09 15:13:02 +0100

    Base slider works with arrow. No active circles.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Provare a riprodurre uno slider di immagini, già fatto in precedenza con jQuery, questa volta con Vue, quindi non manipolando direttamente il DOM, ma i dati di Vue, come fatto stamattina insieme.
+// Bonus: aggiungiamo autoplay.
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            imageIndex: 0,
+            images: [
+                "https://wallpapercave.com/wp/XMnhQSq.jpg",
+                "https://wallpapercave.com/wp/YKJtSPc.jpg",
+                "https://wonderfulengineering.com/wp-content/uploads/2014/07/Landscape-wallpapers-21.jpg",
+                "https://eskipaper.com/images/landscape-wallpaper-hd-28.jpg",
+                "https://wallpapercave.com/wp/mqgLomk.jpg"
+            ],
+        },
+        methods: {
+            nextImage: function() {
+                this.imageIndex++;
+
+                if(this.imageIndex == this.images.length) {
+                    this.imageIndex = 0;
+                }
+            },
+            prevImage: function() {
+                this.imageIndex--;
+
+                if(this.imageIndex == - 1) {
+                    this.imageIndex = this.images.length - 1;
+                }
+            },
+        },
+    }
+);
